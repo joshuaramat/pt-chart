c13f18c4cbb6333849c42d7a7dc1cddd29bf40bd
Joshua Ramat <ramatjoshua@gmail.com>
2023-03-29 20:15:09 -0700

    updated Chart component to match the styling of the Hero component

--- src/data/patientData.js
+++ src/data/patientData.js
@@ -1,12 +1,14 @@
 const patientData = {
-  labels: ["7:00 AM", "8:00 AM", "9:00 AM", "10:00 AM", "11:00 AM", "12:00 PM", "1:00 PM", "2:00 PM", "3:00 PM", "4:00 PM", "5:00 PM", "6:00 PM", "7:00 PM"],
+  labels: ["7:00 AM", "8:00 AM", "9:00 AM", "10:00 AM", "11:00 AM", "12:00 PM", "1:00 PM", "2:00 PM", "3:00 PM", "4:00 PM", "5:00 PM", "6:00 PM"],
   datasets: [
     {
       label: "Patient Volume Today",
       data: [],
-      backgroundColor: "rgba(54, 162, 235, 0.2)",
-      borderColor: "rgba(54, 162, 235, 1)",
+      backgroundColor: "rgba(0, 123, 255, 0.8)",
+      borderColor: "rgba(0, 87, 162, 1)",
       borderWidth: 1,
+      hoverBackgroundColor: "rgba(0, 87, 162, 1)",
+      hoverBorderColor: "rgba(0, 39, 76, 1)",
     },
   ],
 };
